aa68788b198c761aa48c2c197b9fd9ffe75eff43
agdt3 <pavel.a@aol.com>
2017-08-07 19:06:18 -0400

    moved out sidebar

--- media/involved.php
+++ media/involved.php
@@ -140,48 +140,7 @@
             </form>
           </div>
         </div>
-        <div class="sidebar-wrapper column is-2-desktop is-4-tablet">
-          <a class="button sidebar-button is-danger is-fullwidth has-text-centered is-hidden-mobile">
-            Give to the campaign
-          </a>
-          <div class="box update-form-wrapper">
-            <form id="update-form">
-              <h4 class="form-caption has-text-centered title is-4">
-                Get Updates!
-              </h4>
-							<div class="field">
-								<div class="control has-icons-left">
-									<input class="input is-info" type="email" placeholder="Email">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
-                  </span>
-								</div>
-							</div>
-							<div class="field">
-								<div class="control has-icons-left">
-									<input class="input is-info" type="text" placeholder="Zip Code">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-address-card"></i>
-                  </span>
-								</div>
-							</div>
-              <div class="field">
-								<div class="control">
-                  <button class="button is-warning is-fullwidth">Register</button>
-								</div>
-							</div>
-            </form>
-          </div>
-          <a class="button sidebar-button is-warning is-fullwidth has-text-centered is-hidden-mobile">
-            Volunteer
-          </a>
-          <div class="box">
-            Twitter box
-          </div>
-          <div class="box">
-            Facebook box
-          </div>
-        </div>
+        <?php include_once "sidebar.php"; ?>
       </div>
     </section>
     <?php include_once "footer.php"; ?>
---
+++ media/sidebar.php
@@ -0,0 +1,46 @@
+<div class="column sidebar-wrapper is-2-desktop is-4-tablet">
+  <a class="button sidebar-button is-danger is-fullwidth has-text-centered is-hidden-mobile">
+    Give to the campaign
+  </a>
+  <div class="box update-form-wrapper">
+    <form id="update-form">
+      <h4 class="form-caption has-text-centered title is-4">
+        Get Updates!
+      </h4>
+      <div class="field">
+        <div class="control has-icons-left">
+          <input class="input is-info" type="email" placeholder="Email">
+          <span class="icon is-small is-left">
+            <i class="fa fa-envelope"></i>
+          </span>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control has-icons-left">
+          <input class="input is-info" type="text" placeholder="Zip Code">
+          <span class="icon is-small is-left">
+            <i class="fa fa-address-card"></i>
+          </span>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <button class="button is-warning is-fullwidth">Register</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <a class="button sidebar-button is-warning is-fullwidth has-text-centered is-hidden-mobile">
+    Volunteer
+  </a>
+  <div class="box">
+    <a class="twitter-timeline"
+        href="https://twitter.com/votevandermaas">
+      Tweets by @votevandermaas
+    </a>
+  </div>
+  <div class="box">
+    <div class="fb-page" data-href="https://www.facebook.com/votevandermaas2018/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/votevandermaas2018/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/votevandermaas2018/">Votevandermaas</a></blockquote>
+    </div>
+  </div>
+</div>
