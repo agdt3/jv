9eeaea966e24e29c7cd6bbc763d3c6b097195d27
agdt3 <pavel.a@aol.com>
2017-10-12 05:56:44 -0700

    jv changes made to issues.php

--- media/issues.php
+++ media/issues.php
@@ -31,13 +31,20 @@
             <a class="anchor-tag" id="healthcare"></a>
             <article>
               <h1 class="title is-1">Healthcare</h1>
-              <p>We ALL have a right to affordable and cost-effective healthcare - it can be a reality. We can strengthen and expand Medicare while lowering costs. We will stabilize the market by fixing the current law, have Congress pay pay what they promised, then reduce more costs by allowing Medicare to negotiate drug prices and other services. </p>
+              <p>We ALL have a right to affordable and cost-effective healthcare - it can be a reality. We can strengthen and expand Medicare in phases, covering more people while lowering costs. We can stabilize the market by fixing the current laws, have Congress actually pay what they promised, then reduce more costs by repealing the Republican's prescription law that forces us to pay full price, then allowing Medicare to negotiate down drug prices and other services. </p>
               <a class="article-learn-more button is-link" data-id="healthcare">Learn more...</a>
               <div class="extra-content is-hidden" data-id="healthcare">
-                <p>* Fix the flaws in the existing law to keep all insured </p>  
-                <p>* Continue 're-insurance' subsidies to keep providers on the exchanges and prices down </p> 
-                <p>* End legislative sabotage of the ACA </p>
-              <p>Creating an American version of "universal healthcare" starting with Medicare as the perfect tool to model, and creating what I am calling "Consolidated-Medicare", will make it work for all of us. This approach is far more affordable for all, including the taxpayers, and shows that cost-effective healthcare can be a reality.  Don’t be fooled or believe the lies of those who think we’re copying other countries or that don’t believe healthcare is a right; using ‘Consolidated-Medicare’ as a Single Payer/Universal ‘type of’ Healthcare is and will be uniquely American, is an essential investment in our infrastructure, in our economy, our middle-class and an ideal solution for lowering costs and taxes!<p>
+          <p> Think of your family, your home, everything you've worked so hard for; and that if just one loved one was in a serious accident or illness, the medical bills would take it all away!</p>
+             <br/>
+             <p> What To Do?
+                <p><li>Fix the flaws in the existing law to keep all insured and providers solvent</p>  
+                <p><li>Continue 're-insurance' subsidies to keep providers on the exchanges and prices down </p> 
+                <p><li>End legislative sabotage of the ACA </p>
+                <p><li>Creating a healthcare system that works for us ALL - "Consolidated-Medicare". </p>
+             </p>
+         <br/>
+         <p> Our current Medicare is the perfect model to start from, and through a 'phase in over time' approach, we can make it work for all of us. This approach is far more affordable for all, including the taxpayers, and shows that cost-effective healthcare can be a reality.  Don’t be fooled or believe the lies of those who say we’re copying other countries 'socialized medicine' or 'waiting in long lines'- which we already do! And definitely don't listen to those who don’t believe that healthcare is a right; Our 'right to life' in this country is a right to a healthy life!  ‘Consolidated-Medicare’ is and will always be uniquely American, and will save lives and tax dollars! So please do not be fooled by those few greedy rich people at the top 1% who are trying to get more, but at our expense; the Republican healthcare plan gives them a tax cut and more wealth, and we loose our healthcare because we can't afford what's left, and yes, many may die from a lack of healthcare...  Something uniquely American is our compassion & humanity for our fellow man, our natural urge to help others, and they are trying take that away from us - to make us angry that others who are in need or less fortunate, to destroy our humanity - This we must fight with everything that we are! </p>
+              <br/>
               <p>Medicare is very functional, affordable, respected, and creating "Consolidated-Medicare" makes it work for all of us.</p>
               </div>
             </article>
@@ -45,55 +52,63 @@
             <a class="anchor-tag" id="public_lands"></a>
             <article>
               <h1 class="title is-1">Public Lands</h1>
-              <p>Idaho is NOT for sale! Keep public lands public and protected, accessible for hunting, recreation, and your enjoyment. Idaho cannot afford to maintain & protect these these lands and would have to sell them - then they would be inaccessible & gone forever - We cannot let this happen! </p>
+              <p>Idaho is NOT for sale! Keep public lands public and protected, accessible for hunting, recreation, and your enjoyment. Idaho state government cannot afford to maintain & protect these these lands like the Federal government does for us; they already know this, and would have to sell our lands - then they would be inaccessible & gone forever - We cannot let this happen! </p>
               <a class="article-learn-more button is-link" data-id="public-lands">Learn more...</a>
               <div class="extra-content is-hidden" data-id="public-lands">
-                <p>Republicans try to convince us our federal public lands are a "states rights" issue, and that we need to return them to state control – this makes NO sense! Why take on this additional cost and responsibility when the federal government is paying a bill Idaho can’t afford?</p>
-                <ul>
-                  <li>The Federal government owns and pay to manage 32,621,631 acres in Idaho, which is 61.6% of land in the state, caring for the land that we enjoy.</li>
-                  <li>A recent study reveals Idaho would experience up to a $250 million deficit if Idaho owned and had to pay to manage this land; enough to cover the costs of timber mgmt., recreation and road maintenance, or fire protection – which in 2015 alone cost over $300 million, 70% of which the federal government paid. Imagine Idaho being responsible for that entire $300 million.</li>
-                  <li>The Republican political stranglehold on Idaho is also giving away access to our public lands to Texas gas companies and giving away mineral rights of private landholders by passing laws written by the gas industry lobbyists, using ‘forced-pooling’ to steal resources and our property rights!</li>
-                  <li>Republicans are repealing laws that make our rivers save for fishing, for drinking water, and creating laws that put our ground water at risk.</li>
+                <p>Many Republicans try to convince us our federal public lands are a "states rights" issue, and that we need to return them to state control – as though we can do it better - this makes absolutely NO sense... Idaho just doesn't have the money!  So why take on this additional cost and responsibility when the federal government is paying a bill Idaho can’t afford? Because many rich and special business interests want our lands for the profit they can make!</p>
+             <br/><l>
+             <p><li>The Federal government owns and pays to manage 32,621,631 acres in Idaho, which is 61.6% of the land in our state, caring for and maintaining the land that we enjoy.</li></p>
+                  <li>  A recent study reveals Idaho would experience up to a $250 million deficit if Idaho owned and had to pay to manage this land; enough to cover the costs of timber mgmt., recreation and road maintenance, or fire protection – which in 2015 alone cost over $300 million (70% of which the federal government paid).  Imagine Idaho being responsible for that entire $300 million.</li>
+                  <li>  The Republican political stranglehold on Idaho is also giving away access to our public lands to Texas gas companies and giving away mineral rights of private landholders by passing laws written by the gas industry lobbyists, using ‘forced-pooling’ to steal resources and our property rights!</li>
+                  <li>  Republicans are repealing laws that make our rivers save for fishing, for drinking water, and creating laws that put our ground water at risk.</li>
+              <br/>
+             <p> Private Lands - We must also protect private ownership rights - Our Idaho legislature is giving Alta-Mesa a free ‘fracking’ license to take our privately held mineral rights to our own land, using 'forced pooling' to force land owners into leases that are only paying a pittance, and no protection when the land or groundwater for wells are poisoned by the chemicals released.</p>
+                  <li> Since fracking - Oklahoma has more than 3 times the earthquakes as California.</li>
+              <br/><l>
+Useful links that help explain; 
                   <li><a href="http://www.spokesman.com/stories/2017/feb/05/push-to-turn-over-federal-lands-to-the-states-may-">http://www.spokesman.com/stories/2017/feb/05/push-to-turn-over-federal-lands-to-the-states-may-</a></li>
                   <li><a href="https://ballotpedia.org/Federal_land_ownership_by_state">https://ballotpedia.org/Federal_land_ownership_by_state</a></li>
                   <li><a href="https://ballotpedia.org/Environmental_policy_in_Idaho">https://ballotpedia.org/Environmental_policy_in_Idaho</a></li>
-                </ul>
+                  <li><a href="http://www.idahostatesman.com/opinion/readers-opinion/article166403167.html">http://www.idahostatesman.com/opinion/readers-opinion/article166403167.html</a></li>
+                  <li><a href="http://integrityandaccountability.org/forced-pooling.html">http://integrityandaccountability.org/forced-pooling.html</a></li>
+                  <li><a href="http://www.mineralweb.com/news/idaho-pooling-now-law/">http://www.mineralweb.com/news/idaho-pooling-now-law/</a></li>
+                </l>
               </div>
             </article>
             <hr/>
             <a class="anchor-tag" id="education"></a>
             <article>
               <h1 class="title is-1">Education</h1>
-              <p>Work to implement 'Scholar-Quest Program' - Reform education to focus from Pre-K to preparing students for careers, whether a vocational or college degree. Build relevant curriculum with input from industry leaders. Make college free through revenue neutral tax deductions, incentives and reforms. Require graduates to perform community service ‘pay-back’. Unleash economic activity by eliminating student debt. Education is an essential investment our nation’s future, the key to our long-term economic success and security.</p>
+              <p>I commit to treating education as though it is one of our most valuable assets - Which it is! I will work to reform legislative support of education to focus on the children, from Pre-K to preparing students for careers, whether a vocational certification or 2 or 4Year college degree. Build relevant curriculum with input from industry leaders. Make college free through revenue neutral tax deductions, incentives and reforms for business and industry. Require graduates to perform community service ‘pay-back’. Unleash tremendous economic activity by eliminating student debt. Our children are an as yet untapped intellectual resource and could do so much more if only provided with the tools for success - Imagine what all our children could do if educated to their potential.  Education is an investment in the future of our children, an essential investment our nation’s future, and the key to our long-term economic success, growth and security.</p>
               <a class="article-learn-more button is-link" data-id="education">Learn more...</a>
               <div class="extra-content is-hidden" data-id="education">
                 <p>Education is an essential investment in our human infrastructure utilizing our untapped intellectual resources, which is the key to rebuilding our middle-class, to creating a truly free-market, to our long term success in this now global economy, and to our Nation’s future and economic security.</p>
-                <ul>
-                  <li>"Pre-K through 12-Prep", and an educational approach more consistent with the way we learn, and designed to help students identify and pursue the career choice they desire.</li>
-                  <li>"STEAM" instead of STEM; science, technology, engineering, arts, and math.
-                    <ul>
-                      <li>"STEAM", Cultivating and exposing students and adults to the arts and music is known to stimulate creativity in many areas, even outside the arts in the other areas of study.</li>
-                    </ul>
-                  </li>
-                  <li>"Pre-K…"; the young mind learns far sooner than many thought, so why not teach early and see what they retain?</li>
-                  <li>"..to 12-Prep"; We currently try to prepare ALL high school kids for a 4-year college education, and provide little to no support for any job or educational choices in between – not all of our kids are college bound, and we must stop that pressure that makes them feel somehow less of a person because college is not for them!
-                    <ul>
-                      <li>starting from 10th grade, "12-Prep" will offer students preparatory alternatives for the many vocational, 2-year and 4-year degree programs and career choices, feeding them into the Scholar-Quest Program.</li>
-                    </ul>
-                  <li>
+                <br>
+                  "Pre-K through 12-Prep" is an educational approach more consistent with the way we learn, and designed to help students identify and pursue the career choice they desire.</li>
+                  <l>
+                  <li>"Pre-K…"; the young mind learns far sooner than many think, so why not teach early and see what they retain?</li>
+                  <li>"..to 12-Prep"; We currently prepare ALL high school kids for a 4-year college education, and provide little to no support for any job or educational choices in between. Not all of our kids are college bound, yet our current system knows no other goal except to push all our children toward college - and that pressure makes them feel somehow less of a person because many just know that college is not for them! Yet All these children still have tremendous potential if only they were able to pursue an education that fits 'their' abilities. Many of our children want and are best suited for something other than a 4-year college degree, so under my 'Scholar-Quest' program, our children have a choice! </li>
+                  <li>starting from 10th grade, "12-Prep" will offer students preparatory alternatives for the many vocational, 2-year and 4-year degree programs and career choices, feeding them into the Scholar-Quest Program.</li>
                   <li>The total existing student debt is greater than all the credit card debt in America! We must make sure students aren't starting their lives under the crushing burden of debt.</li>
-                </ul>
+                  <li>"STEAM" instead of STEM; science, technology, engineering, arts, and math.
+                    <l>
+                      <li>"STEAM", Cultivating and exposing students and adults to the arts and music is known to stimulate creativity in many areas, even outside the arts in the other areas of study.</li>
+                </l>
               </div>
             </article>
             <hr/>
             <a class="anchor-tag" id="jobs"></a>
             <article>
               <h1 class="title is-1">Jobs and Wages</h1>
-              <p>Create affordable jobs using targeted infrastructure, education, job training, and tax relief plans that both stimulate and grow the economy! Create jobs with wages that allow you to be self-sufficient. Use education, specialized or vocational job training, employment based tax incentives that encourage business to hire, and rationally applied changes to the minimum wage based upon regional cost of living indexes.</p>
+              <p>Create affordable jobs using targeted infrastructure, education, job training, and tax relief plans that both stimulate and grow the economy! Create jobs with wages that allow you to be self-sufficient. Use education, specialized or vocational job training, 'employment based' tax incentives that encourage business to hire & retain, and rationally applied changes to the minimum wage based upon regional cost of living indexes.</p>
               <a class="article-learn-more button is-link" data-id="jobs">Learn more...</a>
               <div class="extra-content is-hidden" data-id="jobs">
-                <p>My goal is to create a more educated and well-trained workforce, tax relief plans that encourage investment that both stimulate and grow the economy and our workforce, while also creating "affordable jobs*". We must also develop targeted infrastructure projects that support future growth and repair or replace existing infrastructure that is failing!</p>
-                <p>Affordable Jobs? These are obs that actually pay respectable wages that afford you the dignity and pride of being self-sufficient, and the ability to live an independent lifestyle. Through rationally applied changes to the minimum wage based upon regional cost of living indexes.</p>
+                <p>My goal is to create a more educated and well-trained workforce, tax relief plans that encourage investment that both stimulate and grow the economy and our workforce, while also creating "affordable jobs*". We must also develop targeted infrastructure projects that support future growth and repair or replace existing infrastructure that is failing!</p><br>
+                <l><p><li>*Affordable Jobs? These are jobs that actually pay respectable wages that afford you the dignity and pride of being self-sufficient, and the ability to live an independent lifestyle. 
+                <li> A realistic 'Living Wage' that is fair to business and the employee, utilizing rationally applied changes to the minimum wage based upon regional cost of living indexes.</li>
+                <li>Create jobs that create jobs! Strategic infrastructure projects that create jobs on their own, but also provide existing and new businesses the room for growth in an expanded 'free market'. Case in point; Install fiber optic/broadband throughout Idaho - Imagine all the new business start-ups and business growth in areas that were formerly inaccessible for today's high-sped business environment!</li>
+                <li>One way to stop businesses from replacing humans with automation or robots - besides a move well educated workforce, create 'employment' and 'employee' based tax incentives that encourage business to hire & retain their employees - No employees, no tax write-offs!</li>
+                </l>
               </div>
             </article>
             <hr/>
