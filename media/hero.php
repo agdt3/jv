bebdc239267afd4eb26bdd07285f1f41ae074f28
jvandermaas <jvandermaas@p3plcpnl0882.prod.phx3.secureserver.net>
2017-10-12 05:47:40 -0700

    jv's changes to hero.php

--- media/hero.php
+++ media/hero.php
@@ -12,30 +12,30 @@
         </div>
     </div>
     <figure class="hero-image active" data-index="1">
-      <img src="images/mountains.jpg"/>
+      <img src="images/James06_Banr_crop.png"/>
       <figure-caption class="container hero-overlay has-text-centered">
         <h2 class="hero-overlay-title title is-2">
-          Mountains!
+          Bringing People Together!
         </h2>
-        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">Mountains are great and we really like them. Come look at some!</p>
+        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">A leader that is one of us, who will speak for ALL of us!</p>
       </figure-caption>
     </figure>
     <figure class="hero-image is-hidden" data-index="2">
-      <img src="images/family.jpg"/>
+      <img src="images/IMG_2096.JPG"/>
       <figure-caption class="container hero-overlay has-text-centered">
         <h2 class="hero-overlay-title title is-2">
-          Family!
+          Why am I Running?
         </h2>
-        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">It's adam and steve, not adam and eve...</p>
+        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">Because I care about our families, the beauty of our land, and that we should be looking out for each other!</p>
       </figure-caption>
     </figure>
     <figure class="hero-image is-hidden" data-index="3">
-      <img src="images/politics.jpg"/>
+      <img src="images/Jim-&-Keith-E_Crop.jpg"/>
       <figure-caption class="container hero-overlay has-text-centered">
         <h2 class="hero-overlay-title title is-2">
-          Old People!
+          Ensuring Idaho has a voice!
         </h2>
-        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">The internet is a series of tubes...</p>
+        <p class="hero-overlay-text subtitle is-4 is-hidden-mobile">Working Together to Make This an Election of Change</p>
       </figure-caption>
     </figure>
   </div>
