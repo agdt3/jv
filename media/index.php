251d5ec545cd8ef219d3172510183384fd25260f
agdt3 <pavel.a@aol.com>
2017-10-15 06:32:52 -0700

    more jv changes

--- media/index.php
+++ media/index.php
@@ -38,62 +38,61 @@
         <div class="column is-6-desktop is-offset-2-desktop is-8-tablet">
           <div class="news-wrapper">
             <h1 class="title is-1">Latest News</h1>
-            <article class="media">
-              <div class="media-left">Jul 19</div>
+             <article class="media">
+              <div class="media-left">Oct 13-15th</div>
               <div class="media-content">
                 <div class="content">
-                  <h2 class="subtitle is-4">Title 1</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
+                  <h2 class="subtitle is-4">'Clint Stennett Fall Meeting & Training'</h2>
+                  <figure class="issues-pic issues-pic-right">
+                  <img src="images/Pocatello_Train_Oct_Web.jpg"></img>
+                </figure>
+                  James is headed to Pocatello for the Clint Stennett Fall Meeting & Training Weekend! He will be attending Saturday and Sunday, and very pleased to be meeting with U.S. Sen. Jeff Merkley from Oregon, who is the keynote speaker for the conference.
+                  </div>
+                </div>
+              </article>
+             <article class="media">
+              <div class="media-left">Oct 6-8th</div>
+              <div class="media-content">
+                <div class="content">
+                  <h2 class="subtitle is-4">Kootenai County Democrats </h2>
+                  <figure class="issues-pic issues-pic-right">
+                  <img src="images/KC Dems.png"></img>
+                </figure>
+                  On his latest trip to North Idaho from October 6th-8th, James Vandermaas met with the Kootenai County Democrat Candidate Recruitment and Support Committee to introduce himself and listen to what Kootenai Democrats are concerned with. Some of the Democrats in attendance included Paula Niels, the Chair of the Kootenai Democrat Central Committee, Richard Rolland, the Vice Chair of the KCDCC, Christopher Matthews, the Chair of the Candidate Recruitment Committee, and Shem Hanks, the President of the Kootenai Democratic Club. James showed genuine knowledge and a precise plan for what he would do if elected in areas of Education, Business Relations, Healthcare, and an ingenious idea of factoring in cost of living factors to better adjust the livable wage for specific communities. James was asked about DACA and the committee was very pleased with his support of these young productive members of our county, who have put themselves out there. When asked how he would respond to moderate and progressive Democrats, Mr. Vandermaas illustrated his plan for a ‘Coalition of Democracy” to bring everyone together to give all Idahoans in Congressional District 1 a voice, one unified voice regardless of whether you are conservative, progressive, Republican, Democrat, or anything in between. James left the meeting with more support and greater excitement in the prospect of a competent, well-organized, and decisive Democrat running for Congress. A serious contender for a serious seat. The committee was very impressed with the ease and knowledge James was able to show when he answered questions on the fly, showing his genuine and decisive nature. The Committee felt confidant that James Vandermaas could bring together Idahoans regardless of party. On Saturday James and Cindy attended “Disco Inferno 2017: 80’s Dance Explosion": the fourth annual benefit for Coeur d’Alene School District 271 school libraries, taking the night to support our Schools.<br>
+<p>Christopher S. Matthews
+Candidate Recruitment/Support Committee Chair</p>
                 </div>
               </div>
             </article>
-            <article class="media">
-              <div class="media-left">Jul 18</div>
+             <article class="media">
+              <div class="media-left">Sept 15th</div>
               <div class="media-content">
                 <div class="content">
-                  <h2 class="subtitle is-4">Title 2</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
-                </div>
+                  <h2 class="subtitle is-4">JFK Banquet</h2>
+                  <figure class="issues-pic issues-pic-right">
+                  <img src="images/Ada_Co_Dems.jpg"></img>
+                </figure>
+                  Sponsored by the Ada County Democrats, this was fundraiser and wonderful social event. Our friend Sam Sandmire did a great job as emcee, and the keynote speaker, Cook County Commissioner Bridget Gainer, gave quite a relevant talk.  Between good friends, good food, and winning one of the silent auction gift baskets, James & Cindy had a great night! 
+                <figure class="issues-pic issues-pic-right">
+                  <img src="images/JFK Banquet_2017.jpg"></img>
+                </figure>
+               </div>
               </div>
             </article>
-            <article class="media">
-              <div class="media-left">Jul 17</div>
+             <article class="media">
+              <div class="media-left">June 3rd</div>
               <div class="media-content">
                 <div class="content">
-                  <h2 class="subtitle is-4">Title 3</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
+                  <h2 class="subtitle is-4">'March for Truth Boise'</h2>
+                  <figure class="issues-pic issues-pic-right">
+                  <img src="images/March4Truth_Jim_Crop.jpg"></img>
+                </figure>
+                  Put together in only 3-4 days, this rally was held on the steps of the Idaho State Capitol.  Speakers included Melissa Wintrow - Idaho Rep. from 19-B, John McCrostie - Idaho Rep. from 16-A, Chase Johnson - Adjunct Professor of Global Studies and Frank Church Institute Research Associate, and James Vandermaas - our candidate for the 1st Congressional District, and was well attended by over 100 concerned citizens. For further info on the 'March for Truth'; https://www.marchfortruth.info/ .
+                  </div>
                 </div>
-              </div>
-            </article>
-            <article class="media is-hidden">
-              <div class="media-left">Jul 16</div>
-              <div class="media-content">
-                <div class="content">
-                  <h2 class="subtitle is-4">Title 4</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
-                </div>
-              </div>
-            </article>
-            <article class="media is-hidden">
-              <div class="media-left">Jul 15</div>
-              <div class="media-content">
-                <div class="content">
-                  <h2 class="subtitle is-4">Title 5</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
-                </div>
-              </div>
-            </article>
-            <article class="media is-hidden">
-              <div class="media-left">Jul 14</div>
-              <div class="media-content">
-                <div class="content">
-                  <h2 class="subtitle is-4">Title 6</h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna libero, elementum sit amet ligula nec, aliquet faucibus est. Mauris tempor iaculis libero vel accumsan. Fusce faucibus orci non pretium elementum. Pellentesque at risus in nunc porta dapibus id at nisl. Mauris ornare fermentum arcu, at gravida quam sagittis at.
-                </div>
-              </div>
-            </article>
-            <p class="has-text-right">
-              <span class="button is-link learn-more-control">Learn more...</span>
+              </article>
+              <p class="has-text-right">
+             <span class="button is-link learn-more-control">More News...</span>
             </p>
           </div>
         </div>
@@ -104,3 +103,4 @@
     <script type="text/javascript" src="/main.js"></script>
   </body>
 </html>
+
