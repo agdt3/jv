89d8dd0aa980cbd3931aac8b0cbd5c6a936a117d
agdt3 <pavel.a@aol.com>
2017-10-18 23:41:51 -0400

    added facebook changes

--- media/sidebar.php
+++ media/sidebar.php
@@ -40,21 +40,20 @@
   </a>
   <?php } ?>
   <?php if ($options && $options['show_district_button']) { ?>
-  <a href="vote.php#district_map" class="button sidebar-button is-fullwidth has-text-centered">
+  <a href="vote.php#district_map" class="button sidebar-button is-fullwidth has-text-centered is-alternate-cta">
     District Map
   </a>
   <?php } ?>
   <?php if ($options && $options['show_twitter_box']) { ?>
-  <div class="box">
-    <a class="twitter-timeline"
-        href="https://twitter.com/votevandermaas">
+  <div class="box sidebar-twitter-container">
+    <a class="twitter-timeline" href="https://twitter.com/votevandermaas">
       Tweets by @votevandermaas
     </a>
   </div>
   <?php } ?>
   <?php if ($options && $options['show_facebook_box']) { ?>
-  <div class="box">
-    <div class="fb-page" data-href="https://www.facebook.com/VoteVandermaas-123623828310861/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/VoteVandermaas-123623828310861/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/VoteVandermaas-123623828310861/">Votevandermaas</a></blockquote>
+  <div class="box sidebar-fb-container">
+    <div class="fb-page" data-href="https://www.facebook.com/VoteVandermaas-123623828310861/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-width="500" data-show-facepile="true"><blockquote cite="https://www.facebook.com/VoteVandermaas-123623828310861/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/VoteVandermaas-123623828310861/">Votevandermaas</a></blockquote>
     </div>
   </div>
   <?php } ?>
