07804a14e7b9a3e777b21e19bbdd1d840aa0857f
agdt3 <pavel.a@aol.com>
2017-10-28 07:56:08 -0400

    added handler

--- media/handler.php
+++ media/handler.php
@@ -1,6 +1,22 @@
 <?php
 include_once "post.php";
 
+function handle_request() {
+  switch ($SERVER['REQUEST_METHOD']) {
+    case 'GET':
+      $resp = handle_get();
+      break;
+    case 'POST'
+      $resp = handle_post();
+      break;
+    default:
+      $resp = handle_unsupported_request();
+      break
+  }
+
+  echo $resp;
+}
+
 function parse_volunteer_data($data_obj) {
   $values = array();
   foreach($data_obj as $key => $value) {
@@ -30,20 +46,30 @@ function response($msg, $code=200) {
   ));
 }
 
+function handle_post() {
+  $data_obj = json_decode(file_get_contents('php://input'), true);
+  $sheet_type = $data_obj['sheet_type'];
+  $data_values = parse_volunteer_data($data_obj);
 
-$data_obj = json_decode(file_get_contents('php://input'), true);
-$sheet_type = $data_obj['sheet_type'];
-$data_values = parse_volunteer_data($data_obj);
+  try {
+    $client = getClient();
+    $service = getService($client);
 
-try {
-  $client = getClient();
-  $service = getService($client);
-
-  $result = insertRow($service, $sheet_type, $data_values);
-  echo response("success", 200);
+    $result = insertRow($service, $sheet_type, $data_values);
+    return response("success", 200);
+  }
+  catch (Exception $e) {
+    return response($e->getMessage(), 500);
+  }
 }
-catch (Exception $e) {
-  echo response($e->getMessage(), 500);
+
+function handle_get() {
+
 }
 
+function handle_unsupported_request() {
+  return response("Method Not Allowed", 405);
+}
+
+handle_request();
 ?>
