5b098a6a47a4b34074af87a560ca078366cff530
agdt3 <pavel.a@aol.com>
2017-10-28 10:03:06 -0400

    factored out articles

---
+++ media/article_fragment.php
@@ -0,0 +1,65 @@
+<article class="media">
+  <div class="media-left">Nov 10-14th</div>
+  <div class="media-content">
+    <div class="content">
+      <h2 class="subtitle is-4"> "Bringing People Together" - James Vandermaas'  Official Announcement Tour</h2>
+      <figure class="issues-pic issues-pic-right">
+        <img src="images/VandermaasLogoMark_idahoStar_Comps1_EP.jpg"></img>
+      </figure>
+      <p>Be prepared to be heard! Jim is planning to officially announce running for the 1st Congressional District of Idaho on Veterans Day weekend.  The Campaign plans to start our announcement 'Bringing People Together/Listening Tour' in Sand Point, then Coeur d'Alene, and continue working southward to, Plummer, Moscow, Lewiston, McCall, Eagle, Meridian, Caldwell & Nampa, New Plymouth/Fruitland area, and ending in Kuna! Stay tuned for more specific details for your area, as we will put out a more detailed itinerary. "Bringing People Together" is so much more than a slogan, and so much more than a campaign message; it is the character and driving concept of our campaign! Most of us want the same things for ourselves, our families, and for Idaho; realizing that our differences are actually smaller than they seem, and that putting those differences aside and voting as one strong block is the solution to winning in 2018!  The Tour details will be posted on our calendar soon - as soon as our calendar is done being built!
+      </p>
+    </div>
+  </div>
+</article>
+<article class="media">
+  <div class="media-left">Oct 13-15th</div>
+  <div class="media-content">
+    <div class="content">
+      <h2 class="subtitle is-4">Clint Stennett Fall Meeting and Training</h2>
+      <figure class="issues-pic issues-pic-right">
+        <img src="images/Pocatello_Train_Oct_Web.jpg"></img>
+      </figure>
+      <p>James went to Pocatello for the Clint Stennett Fall Meeting and Training Weekend! He attended Saturday and Sunday, and was very pleased to meet with U.S. Sen. Jeff Merkley from Oregon, who was the keynote speaker for the conference. James and Jeff hit it off from the start, both very down to earth, sharing family stories as well as agreement on many issues. And both clearly concerned about putting people and their interests before politics. They will be keeping in touch...</p>
+    </div>
+  </div>
+</article>
+<article class="media">
+  <div class="media-left">Oct 6-8th</div>
+  <div class="media-content">
+    <div class="content">
+      <h2 class="subtitle is-4">Kootenai County Democrats</h2>
+      <figure class="issues-pic issues-pic-right">
+        <img src="images/KC Dems.png"></img>
+      </figure>
+      <p>On his latest trip to North Idaho from October 6th-8th, James Vandermaas met with the Kootenai County Democrat Candidate Recruitment and Support Committee to introduce himself and listen to what Kootenai Democrats are concerned with. Some of the Democrats in attendance included Paula Niels, the Chair of the Kootenai Democrat Central Committee, Richard Rolland, the Vice Chair of the KCDCC, Christopher Matthews, the Chair of the Candidate Recruitment Committee, and Shem Hanks, the President of the Kootenai Democratic Club. James showed genuine knowledge and a precise plan for what he would do if elected in areas of Education, Business Relations, Healthcare, and an ingenious idea of factoring in cost of living factors to better adjust the livable wage for specific communities.</p>
+      <p>James was asked about DACA and the committee was very pleased with his support of these young productive members of our county, who have put themselves out there. When asked how he would respond to moderate and progressive Democrats, Mr. Vandermaas illustrated his plan for a ‘Coalition of Democracy” to bring everyone together to give all Idahoans in Congressional District 1 a voice, one unified voice regardless of whether you are conservative, progressive, Republican, Democrat, or anything in between.</p>
+      <p>James left the meeting with more support and greater excitement in the prospect of a competent, well-organized, and decisive Democrat running for Congress. A serious contender for a serious seat. The committee was very impressed with the ease and knowledge James was able to show when he answered questions on the fly, showing his genuine and decisive nature. The Committee felt confidant that James Vandermaas could bring together Idahoans regardless of party. On Saturday James and Cindy attended “Disco Inferno 2017: 80’s Dance Explosion": the fourth annual benefit for Coeur d’Alene School District 271 school libraries, taking the night to support our Schools.</p>
+<p>-Christopher S. Matthews, Candidate Recruitment/Support Committee Chair</p>
+    </div>
+  </div>
+</article>
+<article class="media">
+  <div class="media-left">Sept 15th</div>
+  <div class="media-content">
+    <div class="content">
+      <h2 class="subtitle is-4">JFK Banquet</h2>
+      <figure class="issues-pic issues-pic-right">
+        <img src="images/Ada_Co_Dems.jpg"></img>
+        <img src="images/JFK Banquet_2017.jpg"></img>
+      </figure>
+      <p>Sponsored by the Ada County Democrats, this was fundraiser and wonderful social event. Our friend Sam Sandmire did a great job as emcee, and the keynote speaker, Cook County Commissioner Bridget Gainer, gave quite a relevant talk.  Between good friends, good food, and winning one of the silent auction gift baskets, James and Cindy had a great night!</p>
+    </div>
+  </div>
+</article>
+<article class="media">
+  <div class="media-left">June 3rd</div>
+  <div class="media-content">
+    <div class="content">
+      <h2 class="subtitle is-4">March for Truth Boise</h2>
+      <figure class="issues-pic issues-pic-right">
+        <img src="images/March4Truth_Jim_Crop.jpg"></img>
+      </figure>
+      <p>Put together in only 3-4 days, this rally was held on the steps of the Idaho State Capitol.  Speakers included Melissa Wintrow - Idaho Rep. from 19-B, John McCrostie - Idaho Rep. from 16-A, Chase Johnson - Adjunct Professor of Global Studies and Frank Church Institute Research Associate, and James Vandermaas - our candidate for the 1st Congressional District, and was well attended by over 100 concerned citizens. For further info on the 'March for Truth'; <a href="https://www.marchfortruth.info/">https://www.marchfortruth.info/</a></p>
+    </div>
+  </div>
+</article>
