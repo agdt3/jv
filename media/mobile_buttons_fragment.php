93468bb5c1aefde1117e0a115da06406ccb18173
agdt3 <pavel.a@aol.com>
2017-11-06 09:42:08 -0500

    endorse button css

--- media/mobile_buttons_fragment.php
+++ media/mobile_buttons_fragment.php
@@ -5,7 +5,7 @@
   <a href="involved.php" class="mobile-top-button mobile-involve-button button is-large is-fullwidth has-text-centered is-warning">
     Get involved
   </a>
-  <a href="about.php#endorsement" class="mobile-endorsement-button button is-large is-fullwidth has-text-centered">
+  <a href="about.php#endorsement" class="mobile-endorsement-button button is-large is-fullwidth has-text-centered is-grey-cta">
     Endorse James
   </a>
 </container>
