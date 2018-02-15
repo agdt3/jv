80f9c7ae6b9139591466dcd58817c8b7433d3595
agdt3 <pavel.a@aol.com>
2018-02-15 18:57:47 -0500

    moved some stories around

--- media/article_fragment.php
+++ media/article_fragment.php
@@ -1,4 +1,71 @@
 <article class="article-container columns is-desktop">
+              <div class="column is-2-desktop">Dec 4th, 2017</div>
+               <div class="column is-10-desktop is-12-tablet">
+                <div class="content">
+                 <h2 class="subtitle is-4">Congressional Republicans to reconcile differences between the House and the Senate versions of the new 'Welfare for the Wealthy Program' tax bills!</h2> 
+                 <a href=https://www.washingtonpost.com/news/business/wp/2017/12/04/brady-to-lead-reconciliation-of-house-and-senate-tax-bills-as-gop-looks-for-fast-answers/?utm_term=.15ac5440c56c target="_blank">Re. Brady (R-Tx) to lead reconciliation of House and Senate tax bills as GOP looks for fast answers</a><p></p>
+                <figure class="issues-pic issues-pic-right">
+                  <img src="images/WaPo_Brady_Ryan_TaxBill.jpg"></img>
+                </figure>
+         <p>Republicans in the House on Monday designated members to serve on the “conference committee” that will sort through tax bill differences with Senate colleagues. Senate Republicans are expected to appoint their members later this week.</p>
+            <p>Congress is attempting to gut America and the Middle-Class! PLEASE get this straight - This hurts ALL Idahoans; both Democrat & Republican!</p>
+<p>Both House & Senate versions of the Republican tax bill remove deductions that benefit us all. They also demonstrate 'debt neutral' hypocrisy, because suddenly increasing the debt is okay. This tax bill increases the debt, which opens a legal 'backdoor' to make cuts to Medicare and Social Security, at the expense of the sick and elderly - Our people! This implements a new 'Welfare for the Wealthy' Program, where they 62% of the financial benefit from this bill to support the wealthy top 1% - ALL at the expense of the poor and middle class! And the Republicans refused to allow the Democrats to read the bill. The final bill is over 400 pages, with 'many' scribbled notes in the margins, so which Republicans actually read the whole bill?  Democrats had to learn details from lobbyists, and only a couple hours before the vote!</p>
+          <a class="article-learn-more button is-link" data-id="congressional">Learn what you can do...</a>
+              <div class="extra-content is-hidden" data-id="congressional">
+              <div><p>The U.S. House of Representatives Conference members involved in reconciliation of the tax bill are as follows – PLEASE hit the phones, over and over again. Let’s build this into our daily routines, coffee, then call, sip, call, sip, call...!
+         <l>
+         <li>Rep. Kevin Brady (R-TX)
+District office phone numbers: (936) 441-5700 | (936) 439-9532</li>
+          <li>Rep. Devin Nunes (R-CA) 
+District office phone numbers: (559) 733-3861 | (559) 323-5235</li>
+          <li>Rep. Peter Roskam (R-IL)
+District office phone number: (630) 323-0006 </li>
+          <li>Rep. Diane Black (R-TN)
+District office phone numbers: (615) 206-8204 | (931) 854-0069</li>
+          <li>Rep. Kristi Noem (R-SD)
+District office phone numbers: (605) 878-2868 | (605) 275-2868 | (605) 791-4673</li>
+          <li>Rep. Rob Bishop (R-UT)
+District office phone number: (801) 625-0107</li>
+          <li>Rep. Don Young (R-AK)
+District office phone numbers: (907) 271-5975 | (907) 456-0210 | (907) 586-7400</li>
+          <li>Rep. Greg Walden (R-OR)
+District office phone numbers: (541) 776-4646 | (541) 389-4408 | (541) 624-2400</li>
+          <li>Rep. John Shimkus (R-IL)
+District office phone numbers: (618) 288-7190 | (217) 347-7947 | (618) 252-8271 | (217) 466-0664</li></l>
+              <p></p>
+              <p>You can also help by going to www.VoteVandermaas.com, and clicking on the little red button in the top right corner!</p>
+                </div>
+</article>
+<article class="article-container columns is-desktop">
+              <div class="column is-2-desktop">Dec 3rd, 2017</div>
+               <div class="column is-10-desktop is-12-tablet">
+                <div class="content">
+                 <h2 class="subtitle is-4">Fulcher gets endorsement from the Koch brothers SuperPAC!</h2> 
+                 <a href=http://www.spokesman.com/blogs/boise/2017/dec/03/crapo-parts-bowles-simpson-over-gop-tax-bill-deficit/?fb_comment_id=1761593370549347_1762966387078712&comment_id=1762966387078712#f29235cf4442b88 target="_blank">The Spokesman-Review: "Fulcher gets superPAC backing"</a><p>(2nd article down)</p>
+                <figure class="issues-pic issues-pic-right">
+                  <img src="WaPo_Brady_Ryan_TaxBill.jpg"></img>
+                </figure>
+                <p>FreedomWorks, originally formed as 'Citizens for a Sound Economy' in 1984 by brothers Charles and David Koch, is national advocacy and campaign group associated with the tea party has announced its endorsement of Russ Fulcher in the race for Idaho’s open 1st District congressional seat in 2018.</p>
+            <p>We can stand up to the Koch brothers and the top 1%, but we must resist their efforts to divide us, and stand united against this threat to our way of life and our country. This is not a battle about Party, but a battle for ALL Idahoans - both Democrats and Republicans are being hurt. And it is a battle we can win if we come together to protect our schools, healthcare, public lands...  You can also help by clicking on the little red button in the top right corner!</p>
+          <p><a href="http://www.factcheck.org/2012/04/freedomworks-for-america/" target="_blank"><strong> >Reality Check? = FactCheck.org - FreedomWorks for America</strong></a></p>
+                   <br>
+                 </div>
+</article>
+<article class="article-container columns is-desktop">
+              <div class="column is-2-desktop">Dec 1st 2017</div>
+               <div class="column is-10-desktop is-12-tablet">
+                <div class="content">
+                 <h2 class="subtitle is-4">Michael Flynn Pleads Guilty - Signals Deal with Prosecution</h2> 
+                 <a href=https://www.nytimes.com/2017/12/01/us/politics/michael-flynn-guilty-russia-investigation.html target="_blank">NYT: Michael Flynn Pleads Guilty to Lying to the F.B.I.</a><p></p>
+      <figure class="issues-pic issues-pic-right">
+        <iframe title="New York Times Video - Embed Player" width="480" height="321" frameborder="0" scrolling="no" allowfullscreen="true" marginheight="0" marginwidth="0" id="nyt_video_player" src="https://static01.nyt.com/video/players/offsite/index.html?videoId=100000005581610"></iframe>
+      </figure>
+      <p>The Mueller investigation is well within Trump's White House Administration. Though don't know the full details of Michael Flynn's plea agreement, he has pleaded guilty to a felony - <i>lying to the FBI regarding his contact with Russian agents</i> - and what that contact was about. A 'plea deal' means that many other criminal charges do exist, but are not being pursued, this in exchange for Flynn sharing his knowledge of the criminal activity of others. As one who lived through the 'Cuban Missile Crisis', knowing that the threat from Russia was very real, I was one of millions of kids made to hide under our school desks once or twice a week for the national air raid drills, and had to stay there until the 'all clear' siren.  As though that would save us from a nuclear strike!  And also one who can see that those missiles are actually flying now - just in the form of Facebook posts, Tweets, and state voter file hacking, using propaganda to spread lies, distortions and misinformation. And Michael Flynn was apparently complicit! General Flynn, who served with distinction in our armed forces and intelligence (DIA), for some reason since retiring, has worked for and aided adversarial foreign powers against the interests and security of the United States, and received a payment from the Russian government-funded news outlet RT in 2015 after being told by the military that he could not accept money from foreign governments in his retirement. There was a day that the word 'traitor' would have been utter by all, regardless of ideology. This calls into serious question whether the Army should investigate if warrants him being stripped of his rank & pension.</p>
+             <p><a href="http://www.VoteVandermaas.com/images/Flynn-18-USC-1001-a-2-Indictment.pdf" target="_blank"><strong> > Flynn;18-USC-1001 Criminal Indictment</strong></a></p>
+             <p><a href="http://www.VoteVandermaas.com/images/Flynn-Plea-Agreement.pdf" target="_blank"><strong> > Flynn;Plea Agreement</strong></a></p>
+             <p><a href="http://www.VoteVandermaas.com/images/flynn_statement_of_offense.pdf" target="_blank"><strong> > Flynn; Statement of the Criminal Offense</strong></a></p>
+</article>
+<article class="article-container columns is-desktop">
   <div class="column is-2-desktop">Nov 10-14th</div>
   <div class="column is-10-desktop is-12-tablet">
     <div class="content">
